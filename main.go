8308d3a5a53994f0d9f414129f36134c22499e38
sanix <sanicksikani@gmail.com>
2021-04-09 14:36:14 +0300

    url shortener

--- main.go
+++ main.go
@@ -1,16 +1,91 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 
-	"github.com/go_postgres/helper"
+	"github.com/go_postgres/base62"
+	"github.com/go_postgres/models"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	_, err := helper.InitDB()
+type DBClient struct {
+	db *sql.DB
+}
+
+type Record struct {
+	ID  int    `json:"id"`
+	URL string `json:"url"`
+}
+
+//GenerateShortURL adds URL to DB and gives back shortened string
+func (driver *DBClient) GenerateShortURL(w http.ResponseWriter, r *http.Request) {
+	var id int
+	var record Record
+	postBody, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(postBody, &record)
+	err = driver.db.QueryRow("INSERT INTO web_url(url) VALUES($1) RETURNING id", record.URL).Scan(&id)
+	responseMap := map[string]string{"encoded_string": base62.ToBase62(id)}
+
 	if err != nil {
-		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(responseMap)
+		w.Write(response)
+	}
+}
+
+//GetOriginalURL fetches the original URL for the given encoded(short) string
+func (driver *DBClient) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
+	var url string
+	vars := mux.Vars(r)
+	//Get Id from base62 string
+	id := base62.ToBase10(vars["encoded_string"])
+	err := driver.db.QueryRow("SELECT url FROM web_url WHERE id = $1", id).Scan(&url)
+	//Handle response details
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		responseMap := map[string]interface{}{"url": url}
+		response, _ := json.Marshal(responseMap)
+		w.Write(response)
+	}
+}
+
+func main() {
+	db, err := models.InitDB()
+	if err != nil {
+		panic(err)
 	}
 
-	log.Println("Database tables are successfully initialized.")
+	dbclient := &DBClient{db: db}
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//Create a new router
+	r := mux.NewRouter()
+	//Attach an elegant path with handler
+	r.HandleFunc("/v1/short/{encoded_string:[a-zA-Z0-9]*}",
+		dbclient.GetOriginalURL).Methods("GET")
+	r.HandleFunc("/v1/short", dbclient.GenerateShortURL).Methods("POST")
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		//Good practice: enforce timeouts for servers you create !
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
